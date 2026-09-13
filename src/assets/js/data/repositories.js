f6e1011fc9d865ba1795de926d81b6c373728118
AndreSSFlorez2000 <127819455+AndreSFlorez2000@users.noreply.github.com>
2026-09-13 14:43:55 -0500

    Corregir carga versionada de módulos: src/assets/js/data/repositories.js

--- src/assets/js/data/repositories.js
+++ src/assets/js/data/repositories.js
@@ -10,7 +10,7 @@ import {
   normalizeProjectSummary,
   projectInputToRecord,
   VALIDATION_STATUSES
-} from "./models.js";
+} from "./models.js?v=0.3.0";
 
 export class DataAccessError extends Error {
   constructor(message, cause = null) {
